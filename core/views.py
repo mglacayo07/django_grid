813d44c51173755ef4663ae721fd88a21ee5121d
Maria Lacayo <marialacayo@MacBook-Air.local>
2022-09-01 10:37:51 -0500

    core config done

--- core/views.py
+++ core/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
-# Create your views here.
+
+class HomePageView(TemplateView):
+    template_name = "core/home.html"
+
+    def get(self,request, *args, **kwargs):
+        kw = {
+            'title': "Django grid example",
+            'theme': 'core/themes/redmond/jquery-ui.css'
+        }
+        return render(request, self.template_name, kw)
